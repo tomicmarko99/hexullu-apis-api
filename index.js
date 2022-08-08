45845a3bb40129214a210dbfc183f043c70212fc
unknown <masheritto@gmail.com>
2022-08-08 21:01:48 +0200

    updated

--- index.js
+++ index.js
@@ -10,31 +10,39 @@ var cors = require("cors");
 app.use(cors());
 
 const hexullu = [];
+const interval = setInterval(() => {
+  axios
+    .get("https://rapidapi.com/user/hexulluapis")
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
 
-axios
-  .get("https://rapidapi.com/user/hexulluapis")
-  .then((response) => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-
-    $(".vueperslide--visible", html).each(function () {
-      const title = $(this).find(".ApiName").text();
-      const description = $(this).find(".CardContent", "p").text();
-      const img = $(this).find(".b-avatar-img img").attr("src");
-      const url = `https://rapidapi.com` + $(this).find(".CardLink").attr("href");
-      hexullu.push({
-        title,
-        description,
-        img,
-        url,
+      $(".vueperslide--visible", html).each(function () {
+        const title = $(this).find(".ApiName").text();
+        const description = $(this).find(".CardContent", "p").text();
+        const img = $(this).find(".b-avatar-img img").attr("src");
+        const url = `https://rapidapi.com` + $(this).find(".CardLink").attr("href");
+        hexullu.push({
+          title,
+          description,
+          img,
+          url,
+        });
       });
-    });
 
-    // res.json(hexullu);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+      // res.json(hexullu);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  clearArr(hexullu);
+}, 10000);
+
+const clearArr = (hexullu) => {
+  while (hexullu.length > 0) {
+    hexullu.pop();
+  }
+};
 
 app.get("/", (req, res) => {
   res.json("Welcome to Hexullu Rapid API");
